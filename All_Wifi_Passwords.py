8edd88fd3409bb5db1f9fe500c0503d25f8f4016
Kaivalya Kate <6522kaivalya@gmail.com>
2021-04-10 19:35:01 +0530

    Script to get Passwords of all the networks

---
+++ All_Wifi_Passwords.py
@@ -0,0 +1,35 @@
+import subprocess
+import re
+
+cmd_op = subprocess.run(["netsh","wlan","show","profiles"], stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode()
+
+profiles = (re.findall("All User Profile     : (.*)\r", cmd_op))
+
+list_wifi = list()
+
+if len(profiles) != 0:
+    for name in profiles:
+        each_profile = dict()
+
+        profile_info = subprocess.run(["netsh", "wlan", "show", "profiles", name],
+                                      stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode()
+
+        if re.search("Security key           : Absent", profile_info):
+            continue
+        else:
+            each_profile["ssid"] = name
+            password_profile = subprocess.run(
+                ["netsh", "wlan", "show", "profiles", name, "key=clear"], stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode()
+            password = re.search("Key Content            : (.*)\r", password_profile)
+
+            if password == None:
+                each_profile["password"] = None
+            else:
+                each_profile["password"] = password[1]
+
+            list_wifi.append(each_profile)
+
+for i in list_wifi:
+    print(i)
+            
+
